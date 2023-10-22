2a6aa2985fde5961d355d2f27cf3d7b079cc1cb1
abhinandak1 <2243@tkmce.ac.in>
2023-10-22 14:13:32 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD Lab"
+include(":c1a1")
+include(":c1a2")
+include(":c1a3")
+include(":c2a3")
