6c602bf4b23c71f98fd683073ed78b524a805f76
abhinandak1 <2243@tkmce.ac.in>
2023-12-01 22:28:22 +0530

    Modify CO3 App4

--- CO3/app4/src/main/java/com/co3/app4/MainActivity.java
+++ CO3/app4/src/main/java/com/co3/app4/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,19 +30,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
+        startActivity(new Intent(MainActivity.this, SecondActivity.class));
         if (itemId == R.id.item1) {
-            Toast.makeText(MainActivity.this, "item1", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
             return true;
         } else if (itemId == R.id.item2) {
-            Toast.makeText(MainActivity.this, "item2", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Exit", Toast.LENGTH_SHORT).show();
             return true;
         }
         else if (itemId == R.id.item3) {
-            Toast.makeText(MainActivity.this, "item3", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
             return true;
         }
         else if (itemId == R.id.item4) {
-            Toast.makeText(MainActivity.this, "item4", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Send", Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
